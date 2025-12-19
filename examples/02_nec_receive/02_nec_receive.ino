0066217667716f7ca25f42a6dbbb89cb25b93f60
TANAKA Masayuki <tanakamasayuki@gmail.com>
2025-12-19 18:49:51 +0900

    受信処理の初期設定を整理し、受信フレームの詳細情報をログに出力するように修正

--- examples/02_nec_receive/02_nec_receive.ino
+++ examples/02_nec_receive/02_nec_receive.ino
@@ -9,6 +9,8 @@ esp32ir::Receiver rx(32);
 void setup()
 {
   Serial.begin(115200);
+  rx.setInvertInput(true);
+  rx.useRawPlusKnown();
   rx.addProtocol(esp32ir::Protocol::NEC);
   rx.begin();
 }
@@ -18,9 +20,22 @@ void loop()
   esp32ir::RxResult rxResult;
   if (rx.poll(rxResult))
   {
-    Serial.printf("Received IR frame: protocol=%u status=%u\n",
-                  static_cast<uint16_t>(rxResult.protocol),
-                  static_cast<uint8_t>(rxResult.status));
+    Serial.printf("Received IR frame:\n");
+    Serial.printf(" status=%u\n", static_cast<uint8_t>(rxResult.status));
+    Serial.printf(" protocol=%u\n", static_cast<uint16_t>(rxResult.protocol));
+    Serial.printf(" raw frames=%u totalUs=%u\n",
+                  rxResult.raw.frameCount(), rxResult.raw.totalTimeUs());
+    for (uint16_t i = 0; i < rxResult.raw.frameCount(); ++i)
+    {
+      const esp32ir::ITPSFrame &frame = rxResult.raw.frame(i);
+      Serial.printf("  Frame %u: T_us=%u edges=%u\n    ",
+                    i, frame.T_us, frame.len);
+      for (uint16_t j = 0; j < frame.len; ++j)
+      {
+        Serial.printf("%d ", frame.seq[j]);
+      }
+      Serial.printf("\n");
+    }
 
     esp32ir::payload::NEC nec;
     if (esp32ir::decodeNEC(rxResult, nec))
