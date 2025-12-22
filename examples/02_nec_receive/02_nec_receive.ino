5768b6ac2bc462dbec568f0264e04a21e0a3207d
TANAKA Masayuki <tanakamasayuki@gmail.com>
2025-12-22 18:22:00 +0900

    受信フレームのデバッグ出力を削除し、受信アドレスとコマンドの表示を改善

--- examples/02_nec_receive/02_nec_receive.ino
+++ examples/02_nec_receive/02_nec_receive.ino
@@ -24,20 +24,10 @@ void loop()
   esp32ir::RxResult rxResult;
   if (rx.poll(rxResult))
   {
-    Serial.printf("Received IR frame:\n");
-    Serial.printf(" status=%s\n", esp32ir::util::rxStatusToString(rxResult.status));
-    Serial.printf(" protocol=%s\n", esp32ir::util::protocolToString(rxResult.protocol));
-    Serial.printf(" payloadStorage=%u bytes ", static_cast<unsigned>(rxResult.payloadStorage.size()));
-    for (size_t i = 0; i < rxResult.payloadStorage.size(); ++i)
-    {
-      Serial.printf(" %02X", rxResult.payloadStorage[i]);
-    }
-    Serial.println();
-
     esp32ir::payload::NEC nec;
     if (esp32ir::decodeNEC(rxResult, nec))
     {
-      Serial.printf("addr=0x%04X cmd=0x%02X repeat=%s\n",
+      Serial.printf("Received addr=0x%04X cmd=0x%02X repeat=%s\n",
                     nec.address, nec.command, nec.repeat ? "true" : "false");
     }
     else
@@ -45,5 +35,6 @@ void loop()
       Serial.println("decodeNEC failed");
     }
   }
+
   delay(1);
 }
